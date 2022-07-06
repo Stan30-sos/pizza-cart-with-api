2d87cb0af568483d8416ad45a6776b746ad67eb7
oratilwe <oratilwestan@gmail.com>
2022-07-06 19:04:46 +0200

    called my pizzalist with api

---
+++ pizza-cart-api.js
@@ -0,0 +1,18 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('pizzaCartWithAPIWidget', function() {
+      return {
+        init() {
+
+         axios
+              .get('https://pizza-cart-api.herokuapp.com/api/pizzas')
+              .then((result) => {
+                this.pizzaList = result.data.pizzas
+              })
+
+
+
+        },
+        pizzaList : [],
+      }
+    })
+})
